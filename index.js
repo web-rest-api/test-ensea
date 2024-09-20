4176697366163bccb88ae7209e7567840c56e643
lostmart <lostmart@hotmail.com>
2024-09-20 09:36:43 +0200

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+// declarer une varable
+let firstName = "Bob"
+let age = 34
+
+// changer ça valeur
+firstName = "Michael"
+age = 32
+
+// aficher le valeur dans la console
+console.log(firstName.length)
